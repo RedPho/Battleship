9fbc1baec6a805789f50722cf368bbeb4c12ed6b
RedPho <medasdan@gmail.com>
2023-01-30 01:01:28 +0300

    Add all logic for prep and game phase

--- scripts.js
+++ scripts.js
@@ -1,10 +1,24 @@
+var phase = "preparation";
+let axisDOM = document.getElementById("axis");
+let axis = axisDOM.innerText;
+axisDOM.addEventListener("click", () => {
+  if (axis == "X") {
+    axisDOM.innerText = "Y";
+  }
+  else {
+    axisDOM.innerText = "X";
+  }
+  axis = axisDOM.innerText;
+})
+
+
 function Ship(len = 0, hitpoint = 0, coords = []) {
   function hit() {
     this.hitpoint++;
   }
 
   function isSunk() {
-    return this.hitpoint >= coords.length ? true: false;
+    return this.coords.length == 0 ? true: false;
   }
 
   return {
@@ -26,6 +40,20 @@ function Gameboard() {
   let allPlacedCoords = [];
 
   function placeCarrier(startingCoord, axis) {
+    if (startingCoord[0] > 9 || startingCoord[1] > 9 || startingCoord[0] < 0 || startingCoord[1] < 0) {
+      return;
+    }
+    if (axis == "X") {
+      if (startingCoord[0] > 5) {
+        return;
+      }
+    }
+    if (axis == "Y") {
+      if (startingCoord[1] > 5) {
+        return;
+      }
+    }
+
     carrier.coords.push(startingCoord);
     if (axis == "X") {
       for (let i = 1; i < carrier.len; i++) {
@@ -37,11 +65,33 @@ function Gameboard() {
         carrier.coords.push([startingCoord[0], startingCoord[1]+i]);
       }
     }
+    for (let i = 0; i < allPlacedCoords.length; i++) {
+      for (let j = 0; j < carrier.coords.length; j++) {
+        if (allPlacedCoords[i][0] == carrier.coords[j][0] && allPlacedCoords[i][1] == carrier.coords[j][1]) {
+          carrier.coords = [];
+          return
+        }
+      }
+    }
     allPlacedCoords.push(...carrier.coords);
     return carrier;
   }
 
   function placeBattleship(startingCoord, axis) {
+    if (startingCoord[0] > 9 || startingCoord[1] > 9 || startingCoord[0] < 0 || startingCoord[1] < 0) {
+      return;
+    }
+    if (axis == "X") {
+      if (startingCoord[0] > 6) {
+        return;
+      }
+    }
+    if (axis == "Y") {
+      if (startingCoord[1] > 6) {
+        return;
+      }
+    }
+
     battleship.coords.push(startingCoord);
     if (axis == "X") {
       for (let i = 1; i < battleship.len; i++) {
@@ -53,11 +103,33 @@ function Gameboard() {
         battleship.coords.push([startingCoord[0], startingCoord[1]+i]);
       }
     }
+    for (let i = 0; i < allPlacedCoords.length; i++) {
+      for (let j = 0; j < battleship.coords.length; j++) {
+        if (allPlacedCoords[i][0] == battleship.coords[j][0] && allPlacedCoords[i][1] == battleship.coords[j][1]) {
+          battleship.coords = [];
+          return
+        }
+      }
+    }
     allPlacedCoords.push(...battleship.coords);
     return battleship;
   }
 
   function placeDestroyer(startingCoord, axis) {
+    if (startingCoord[0] > 9 || startingCoord[1] > 9 || startingCoord[0] < 0 || startingCoord[1] < 0) {
+      return;
+    }
+    if (axis == "X") {
+      if (startingCoord[0] > 7) {
+        return;
+      }
+    }
+    if (axis == "Y") {
+      if (startingCoord[1] > 7) {
+        return;
+      }
+    }
+
     destroyer.coords.push(startingCoord);
     if (axis == "X") {
       for (let i = 1; i < destroyer.len; i++) {
@@ -69,11 +141,33 @@ function Gameboard() {
         destroyer.coords.push([startingCoord[0], startingCoord[1]+i]);
       }
     }
+    for (let i = 0; i < allPlacedCoords.length; i++) {
+      for (let j = 0; j < destroyer.coords.length; j++) {
+        if (allPlacedCoords[i][0] == destroyer.coords[j][0] && allPlacedCoords[i][1] == destroyer.coords[j][1]) {
+          destroyer.coords = [];
+          return
+        }
+      }
+    }
     allPlacedCoords.push(...destroyer.coords);
     return destroyer;
   }
 
   function placeSubmarine(startingCoord, axis) {
+    if (startingCoord[0] > 9 || startingCoord[1] > 9 || startingCoord[0] < 0 || startingCoord[1] < 0) {
+      return;
+    }
+    if (axis == "X") {
+      if (startingCoord[0] > 7) {
+        return;
+      }
+    }
+    if (axis == "Y") {
+      if (startingCoord[1] > 7) {
+        return;
+      }
+    }
+
     submarine.coords.push(startingCoord);
     if (axis == "X") {
       for (let i = 1; i < submarine.len; i++) {
@@ -85,11 +179,33 @@ function Gameboard() {
         submarine.coords.push([startingCoord[0], startingCoord[1]+i]);
       }
     }
+    for (let i = 0; i < allPlacedCoords.length; i++) {
+      for (let j = 0; j < submarine.coords.length; j++) {
+        if (allPlacedCoords[i][0] == submarine.coords[j][0] && allPlacedCoords[i][1] == submarine.coords[j][1]) {
+          submarine.coords = [];
+          return
+        }
+      }
+    }
     allPlacedCoords.push(...submarine.coords);
     return submarine;
   }
 
   function placePatrolBoat(startingCoord, axis) {
+    if (startingCoord[0] > 9 || startingCoord[1] > 9 || startingCoord[0] < 0 || startingCoord[1] < 0) {
+      return;
+    }
+    if (axis == "X") {
+      if (startingCoord[0] > 8) {
+        return;
+      }
+    }
+    if (axis == "Y") {
+      if (startingCoord[1] > 8) {
+        return;
+      }
+    }
+
     patrolBoat.coords.push(startingCoord);
     if (axis == "X") {
       for (let i = 1; i < patrolBoat.len; i++) {
@@ -101,9 +217,19 @@ function Gameboard() {
         patrolBoat.coords.push([startingCoord[0], startingCoord[1]+i]);
       }
     }
+    for (let i = 0; i < allPlacedCoords.length; i++) {
+      for (let j = 0; j < patrolBoat.coords.length; j++) {
+        if (allPlacedCoords[i][0] == patrolBoat.coords[j][0] && allPlacedCoords[i][1] == patrolBoat.coords[j][1]) {
+          patrolBoat.coords = [];
+          return
+        }
+      }
+    }
     allPlacedCoords.push(...patrolBoat.coords);
     return patrolBoat;
   }
+
+  
   let hitted = [];
 
   function receiveAttack(coord) {
@@ -114,11 +240,13 @@ function Gameboard() {
         if (ships[i].coords[j][0] == coord[0] && ships[i].coords[j][1] == coord[1]) {
           ships[i].hit();
           hitted.push(coord);
-          ships[i].coords.splice(i, 1);
-          console.log(patrolBoat);
+          ships[i].coords.splice(j, 1);
+          console.log(ships[i]);
+          return true;
         }
       }
     }
+    return false;
   }
 
   function allSunk() {
@@ -161,27 +289,123 @@ function Gameboard() {
   }
 }
 
+let playerBoard = new Gameboard();
+let computerBoard = new Gameboard();
 
-
-function createGameboards() {
-  let playerBoard = document.querySelector(".player-gameboard");
-  let computerBoard = document.querySelector(".computer-gameboard");
+function createGameboardsDOM() {
+  let computerChoices = []
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      computerChoices.push([i, j]);
+    }
+  }
+  let playerBoardUI = document.querySelector(".player-gameboard");
+  let computerBoardUI = document.querySelector(".computer-gameboard");
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
       let newComputerDiv = document.createElement("div");
       newComputerDiv.classList.add("square");
-      newComputerDiv.setAttribute("id", `c${j}${i}`)
+      newComputerDiv.classList.add("c");
+      newComputerDiv.setAttribute("id", `c${j}${i}`);
       let newPlayerDiv = document.createElement("div");
       newPlayerDiv.classList.add("square");
-      newPlayerDiv.setAttribute("id", `p${j}${i}`)
-      computerBoard.appendChild(newComputerDiv);
-      playerBoard.appendChild(newPlayerDiv);
+      newPlayerDiv.classList.add("p");
+      newPlayerDiv.setAttribute("id", `p${j}${i}`);
+      newPlayerDiv.addEventListener("click", () => {
+        if (phase == "preparation") {
+          if (playerBoard.carrier.coords.length == 0) {
+            playerBoard.placeCarrier([j, i], axis);
+            playerBoard.placeShipsInUI();
+            alert("place your battleship");
+          }
+          else if (playerBoard.battleship.coords.length == 0) {
+            playerBoard.placeBattleship([j, i], axis);
+            playerBoard.placeShipsInUI();
+            alert("place your destroyer");
+          }
+          else if (playerBoard.destroyer.coords.length == 0) {
+            playerBoard.placeDestroyer([j, i], axis);
+            playerBoard.placeShipsInUI();
+            alert("place your submarine");
+          }
+          else if (playerBoard.submarine.coords.length == 0) {
+            playerBoard.placeSubmarine([j, i], axis);
+            playerBoard.placeShipsInUI();
+            alert("place your patrol boat");
+          }
+          else if (playerBoard.patrolBoat.coords.length == 0) {
+            playerBoard.placePatrolBoat([j, i], axis);
+            playerBoard.placeShipsInUI();
+            phase = "play";
+          }
+        }
+      })
+      newComputerDiv.addEventListener("click", () => {
+        if (phase == "play") {
+          for (let k = 0; k < computerBoard.clicked.length; k++) {
+            if (computerBoard.clicked[k][0] == j && computerBoard.clicked[k][1] == i) {
+              return;
+            }
+          }
+          newComputerDiv.classList.add("clicked");
+          if (computerBoard.receiveAttack([j,i])) {
+            newComputerDiv.classList.add("hit");
+          }
+          if(computerBoard.allSunk()) {
+            alert("You Win!");
+            location.reload();
+            
+          }
+          let randomIndex = Math.floor(Math.random()*computerChoices.length);
+          let computerChoice = computerChoices[randomIndex];
+          computerChoices.splice(randomIndex, 1);
+          let computerChoiceSquare = document.getElementById(`p${computerChoice[0]}${computerChoice[1]}`);
+          computerChoiceSquare.classList.add("clicked");
+          if (playerBoard.receiveAttack(computerChoice)) {
+            computerChoiceSquare.classList.add("hit");
+          }
+          if (playerBoard.allSunk()) {
+            alert("Computer Wins");
+            location.reload();
+          }
+        }
+      })
+      
+      computerBoardUI.appendChild(newComputerDiv);
+      playerBoardUI.appendChild(newPlayerDiv);
     }
   }
 }
 
-createGameboards();
-let playerBoard = new Gameboard();
-playerBoard.placePatrolBoat([2, 4], "X");
-playerBoard.placeCarrier([5, 5], "X");
-playerBoard.placeShipsInUI();
+function placeShipsRandomForComputer() {
+  while (computerBoard.carrier.coords.length == 0) {
+    let j = Math.floor(Math.random()*10);
+    let i = Math.floor(Math.random()*10);
+    computerBoard.placeCarrier([j, i], axis);
+  }
+  while (computerBoard.battleship.coords.length == 0) {
+    let j = Math.floor(Math.random()*10);
+    let i = Math.floor(Math.random()*10);
+    computerBoard.placeBattleship([j, i], axis);
+  }
+  while (computerBoard.destroyer.coords.length == 0) {
+    let j = Math.floor(Math.random()*10);
+    let i = Math.floor(Math.random()*10);
+    computerBoard.placeDestroyer([j, i], axis);
+  }
+  while (computerBoard.submarine.coords.length == 0) {
+    let j = Math.floor(Math.random()*10);
+    let i = Math.floor(Math.random()*10);
+    computerBoard.placeSubmarine([j, i], axis);
+  }
+  while (computerBoard.patrolBoat.coords.length == 0) {
+    let j = Math.floor(Math.random()*10);
+    let i = Math.floor(Math.random()*10);
+    computerBoard.placePatrolBoat([j, i], axis);
+  }
+  console.log(computerBoard.submarine.coords);
+}
+
+createGameboardsDOM();
+
+placeShipsRandomForComputer()
